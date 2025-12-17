c084b680cc44f813ccc61ad56808980fd5effc55
Nihad Abbasov <nnihat875@gmail.com>
2025-12-18 01:53:45 +0400

    Ilk commit

---
+++ MVCClassTask/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCClassTask.Models;
+using MVCClassTask.ViewModels;
+
+namespace MVCClassTask.Controllers
+{
+    public class HomeController:Controller
+    {
+        public IActionResult Index()
+        {
+            List<Feature> features = new List<Feature>        {
+            new Feature
+            {
+                Id = 1,
+                Title = "Free Shipping Item ",
+                Description = "Orders over $500",
+                Icon = "fas fa-shipping-fast"
+            },
+            new Feature
+            {
+                Id = 2,
+                Title = "Money Back Guarantee",
+                Description = "100% money back",
+                Icon = "fas fa-undo"
+            },
+            new Feature
+            {
+                Id = 3,
+                Title = "Secure Payment",
+                Description = "Safe & secure payment",
+                Icon = "fas fa-lock"
+            },
+            new Feature
+            {
+                Id = 4,
+                Title = "24/7 Support",
+                Description = "Dedicated support",
+                Icon = "fas fa-headset"
+            }
+        };
+            HomeVM homeVM = new HomeVM 
+                {
+                Features = features};
+
+
+
+            return View(homeVM);
+
+        }
+
+    }
+}
---
+++ MVCClassTask/ViewModels/HomeVM.cs
@@ -0,0 +1,10 @@
+﻿using MVCClassTask.Models;
+using System.Drawing;
+
+namespace MVCClassTask.ViewModels
+{
+    public class HomeVM
+    {
+        public List<Feature> Features { get; set; }
+    }
+}
